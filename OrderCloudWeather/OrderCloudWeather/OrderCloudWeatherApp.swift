1c0207193d25c7faf96b40fc7416994a78fc59a0
Tariq Patel <tariqpatel9@gmail.com>
2024-02-16 06:52:48 +0200

    Project creation - Initial commit

---
+++ OrderCloudWeather/OrderCloudWeather/OrderCloudWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderCloudWeatherApp.swift
+//  OrderCloudWeather
+//
+//  Created by Tariq Patel on 2024/02/16.
+//
+
+import SwiftUI
+
+@main
+struct OrderCloudWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
